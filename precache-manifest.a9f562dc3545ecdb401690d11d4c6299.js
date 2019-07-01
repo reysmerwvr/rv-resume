0380c603c78daec2fceda33bea92a2770d3e3084
Reysmer Valle <reysmerwvr@gmail.com>
2019-07-01 13:57:03 -0500

    Updates

---
+++ precache-manifest.a9f562dc3545ecdb401690d11d4c6299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74d26d97edea81b5f41daa2418b121d",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "acbbc0ca5ab93325b0df",
+    "url": "/personal-portfolio/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "41a822c337be38ecbbb7",
+    "url": "/personal-portfolio/static/css/main.c62b8110.chunk.css"
+  },
+  {
+    "revision": "acbbc0ca5ab93325b0df",
+    "url": "/personal-portfolio/static/js/2.a5d1681a.chunk.js"
+  },
+  {
+    "revision": "41a822c337be38ecbbb7",
+    "url": "/personal-portfolio/static/js/main.201f14a5.chunk.js"
+  },
+  {
+    "revision": "199df5cd20e1a679bb0f",
+    "url": "/personal-portfolio/static/js/runtime~main.8fd806ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-portfolio/precache-manifest.9f2e075ec257ab850646cc16355c43f1.js"
+  "/personal-portfolio/precache-manifest.a9f562dc3545ecdb401690d11d4c6299.js"
 );
 
 self.addEventListener('message', (event) => {
