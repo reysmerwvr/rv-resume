d0357ce3eba216f9d8d21f889d1818a3d5f0c6ab
Reysmer Valle <reysmerwvr@gmail.com>
2019-07-01 01:12:13 -0500

    Updates

---
+++ precache-manifest.aaf519b2e0b0cdba5575204369eef54b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd810c5d475cc109b93a5509d0080686",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "1ad49dbe3c5742fbabb5",
+    "url": "/personal-portfolio/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "0010affc51e59eaffd14",
+    "url": "/personal-portfolio/static/css/main.7eb0aff0.chunk.css"
+  },
+  {
+    "revision": "1ad49dbe3c5742fbabb5",
+    "url": "/personal-portfolio/static/js/2.ce599bd1.chunk.js"
+  },
+  {
+    "revision": "0010affc51e59eaffd14",
+    "url": "/personal-portfolio/static/js/main.3f8ba60f.chunk.js"
+  },
+  {
+    "revision": "199df5cd20e1a679bb0f",
+    "url": "/personal-portfolio/static/js/runtime~main.8fd806ec.js"
+  }
+]);
